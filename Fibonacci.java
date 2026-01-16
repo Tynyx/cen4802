6227f8f61f3b8fe383a7fc83ed032b7e3de62bf9
Tynyx <latroyr26@gmail.com>
2026-01-16 20:23:06 -0300

    added Fibonacci sequence

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static long fibonacci(int n) {
+
+        if (n <= 1) {
+            return n;
+        }
+        return (fibonacci(n - 1) + fibonacci(n - 2));
+    }
+    public static void main(String[] args) {
+
+
+
+        System.out.println("The tenth Fibonacci number is: " + fibonacci(10));
+
+    }
+}
